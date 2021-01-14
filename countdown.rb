91c8e1cd523f3554a9320d5c926c3049124ac502
SuperPox <calebkohl@gmail.com>
2021-01-13 17:59:29 -0700

    commit tree 5a367ae521f7cf7225f2aef10d2c56b07f990f59

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
-#write your code here
+#countdown to zero, outputing countdown, happy new year at end, 
 
-def countdown
+def countdown(int)
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        if int == 0
+            return "HAPPY NEW YEAR!"
+        end
+    end
 end
+
+def countdown_with_sleep(int)
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        sleep 1
+        if int == 0
+            return "HAPPY NEW YEAR!"
+        end
+    end
+end
+
